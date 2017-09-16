4acbce12f67f214f39a948a447ddd21acded7079
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-16 12:42:45 +0900

    implement sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000'}
 
   # Do not eager load code on boot.
   config.eager_load = false
